2c878f03eddeec2fa0b03a6ae5bc47a3f096de63
Jeppe <jeppeburchardt@fastmail.com>
2026-06-28 13:26:45 +0200

    Fix text wrapping in special rules table descriptions (#62)

--- src/components/army/SpecialRulesTable.vue
+++ src/components/army/SpecialRulesTable.vue
@@ -18,9 +18,26 @@ defineProps<{
       <tbody>
         <tr v-for="rule in rules" :key="rule.title">
           <td>{{ rule.title }}</td>
-          <td>{{ rule.paragraphs.length > 0 ? rule.paragraphs.join(' ') : '—' }}</td>
+          <td class="rule-description">
+            <p v-for="(paragraph, index) in rule.paragraphs" :key="index">{{ paragraph }}</p>
+            <template v-if="rule.paragraphs.length === 0">—</template>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
+
+<style scoped>
+.army-reference-table .army-reference-table__native-table td.rule-description {
+  white-space: normal;
+}
+
+.rule-description p:not(:last-child) {
+  margin-bottom: 0.5rem;
+}
+
+.rule-description p {
+  margin-top: 0;
+}
+</style>
